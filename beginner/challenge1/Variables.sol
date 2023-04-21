7734fc66205608a13e98a0c0c5d8adb89e178155
charmaine-rabano <75057081+charmaine-rabano@users.noreply.github.com>
2023-04-21 09:42:39 +0800

    Added Beginner course Challenge 1

---
+++ beginner/challenge1/Variables.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract Variables {
+    uint uintVar;
+    bool boolVar;
+    address addressVar;
+    string stringVar;
+
+    function setUintVar(uint _uintVar) public {
+        uintVar = _uintVar;
+    }
+    
+    function setBoolVar(bool _boolVar) public {
+        boolVar = _boolVar;
+    }
+
+    function setAddressVar(address _addressVar) public {
+        addressVar = _addressVar;
+    }
+
+    function setStringVar(string memory _stringVar) public {
+        stringVar = _stringVar;
+    }
+
+    function getUintVar() public view returns(uint) {
+        return uintVar;
+    }
+
+    function getBoolVar() public view returns(bool) {
+        return boolVar;
+    }
+
+    function getAddressVar() public view returns(address) {
+        return addressVar;
+    }
+
+    function getStringVar() public view returns(string memory) {
+        return stringVar;
+    }
+
+}
